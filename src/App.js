18b900210da3d1c63f1372a191d47c5052518ef5
Randy Bellet <rbellet@ecpi.edu>
2026-08-20 08:48:24 -0400

    Initial Railway Application

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Randy Bellet's Railway App
         </a>
       </header>
     </div>
